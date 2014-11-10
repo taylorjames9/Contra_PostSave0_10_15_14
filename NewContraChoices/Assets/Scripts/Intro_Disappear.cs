3f9ae7883d74e3cb4688a704e86531eb7bcf1734
ci3-122-mac2-taylorjames9 <taylorjames9@gmail.com>
2014-11-10 11:23:49 -0600

    Splash screen and small adjustments

---
+++ NewContraChoices/Assets/Scripts/Intro_Disappear.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro_Disappear : MonoBehaviour {
+
+
+	private GameObject myParent;
+
+	// Use this for initialization
+	void Start () {
+		myParent = this.transform.parent.gameObject;
+	}
+	
+
+	void OnMouseDown(){
+		myParent.SetActive (false);
+	}
+}
